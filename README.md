57e5fed405b56580680f0bff6c57d37345dddd9e
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-05-07 16:39:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -23,3 +23,45 @@ $ npm test
 
 ## Usage
 
+```
+'use strict';
+
+var recruitmentsSendMail = require('tfk-recruitment-sendmail');
+
+var options = {
+  apiKey: 'your-sendgrid-api-key',
+  apiUrl: 'https://api.t-fk.no/recruitments',
+  mailTo: 'you@example.com',
+  mailFrom: me@example.com
+};
+
+recruitmentsSendMail(options, function(error, data){
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
+```
+
+Example of returned data
+
+```
+ NumberOfRecruitments: 11,
+  NumberSendt: 11,
+  NumberPreviousSendt: 0,
+  LinksSendt: 
+   [ 'https://hrm.btvregion.no/tfk_recruitment/jobid/2488',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2494',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2489',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2502',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2492',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2501',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2493',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2499',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2434',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2500',
+     'https://hrm.btvregion.no/tfk_recruitment/jobid/2487' ],
+  LinksNotSendt: [] }
+```
+
--- index.js
+++ index.js
@@ -26,16 +26,75 @@ function recruitmentsSendMail(options, callback){
     return callback(new Error('Missing required input: options.mailFrom'), null);
   }
 
+  var totalJobs = 0;
+  var jobsDone = 0;
   var log = {
     NumberOfRecruitments: 0,
     NumberSendt: 0,
     NumberPreviousSendt: 0,
     LinksSendt:[],
     LinksNotSendt: []
+  };
+
+  function areWeDoneYet(){
+    jobsDone ++;
+    if (totalJobs == jobsDone) {
+      return callback(null, log);
+    }
   }
 
-  return callback(null, log);
+  getRecruitments(options.apiUrl, function(error, jobs){
+    if (error) {
+      return callback(error, null);
+    } else {
 
+      log.NumberOfRecruitments = jobs.length;
+      totalJobs = jobs.length;
+
+      jobs.forEach(function(job){
+        db.get(job.jobid, function(err, data){
+          if (err && err.type === 'NotFoundError') {
+
+            createMessage(job, function(er, mail){
+              if(er){
+                return callback(er, null)
+              } else {
+                sendMail({
+                  apiKey: options.apiKey,
+                  to: options.mailTo,
+                  from: options.mailFrom,
+                  subject: mail.subject,
+                  message: mail.message
+                }, function(e, msg){
+                  if(e){
+                    return callback(e, null)
+                  } else {
+                    log.NumberSendt++;
+                    log.LinksSendt.push(job.link);
+                    db.put({key:job.jobid, value:job}, function(feil, putMsg){
+                      if(feil){
+                        return callback(feil, null);
+                      } else {
+                        areWeDoneYet();
+                      }
+                    })
+                  }
+                });
+
+              }
+            });
+
+
+          } else {
+            log.NumberPreviousSendt++;
+            log.LinksNotSendt.push(job.link);
+            areWeDoneYet();
+          }
+        });
+
+      });
+    }
+  });
 }
 
 module.exports = recruitmentsSendMail;
